34af8eec239aa57566b7c49dc6fe0f052717200c
HBSIN\admin <hansberbnard.sinugbuhan@gmail.com>
2021-03-15 08:28:54 +0800

    git first push, v1 simple todo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+         <TodoList />
+    </div>
+  );
+}
+
+export default App;
